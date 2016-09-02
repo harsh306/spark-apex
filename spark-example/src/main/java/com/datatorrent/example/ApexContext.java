ee61cd85836cc25587dd3011999698a7fb930256
bhupeshchawda <bhupesh@apache.org>
2016-09-02 16:28:33 +0530

    Initial commit

---
+++ spark-example/src/main/java/com/datatorrent/example/ApexContext.java
@@ -0,0 +1,14 @@
+package com.datatorrent.example;
+
+import org.apache.spark.SparkContext;
+import org.apache.spark.rdd.RDD;
+
+public class ApexContext extends SparkContext
+{
+
+  public ApexContext()
+  {
+    // TODO Auto-generated constructor stub
+  }
+
+}
