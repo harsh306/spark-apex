dfba92639c62de341b2ef22a8302b6a627c50868
bhupeshchawda <bhupesh@apache.org>
2016-09-22 12:19:57 +0530

    WIP

--- spark-example/src/main/java/com/datatorrent/example/ApexContext.java
+++ spark-example/src/main/java/com/datatorrent/example/ApexContext.java
@@ -19,11 +19,13 @@ public class ApexContext extends SparkContext
   @Override
   public RDD<String> textFile(String path, int minPartitions)
   {
-    ApexRDD rdd = new ApexRDD<>();
+    ApexRDD rdd = new ApexRDD<>(this);
     LineByLineFileInputOperator fileInput = rdd.getDag().addOperator("Input", LineByLineFileInputOperator.class);
     fileInput.setDirectory(path);
     fileInput.setPartitionCount(minPartitions);
     System.out.println(rdd.getDag().toString());
+    rdd.currentOperator = fileInput;
+    rdd.currentOperatorType = ApexRDD.OperatorType.INPUT;
     return rdd;
   }
 }
--- spark-example/src/main/java/com/datatorrent/example/ApexRDD.java
+++ spark-example/src/main/java/com/datatorrent/example/ApexRDD.java
@@ -1,12 +1,15 @@
 package com.datatorrent.example;
 
 import org.apache.spark.Partition;
+import org.apache.spark.SparkContext;
 import org.apache.spark.TaskContext;
 import org.apache.spark.rdd.RDD;
 
 import com.datatorrent.api.DAG;
+import com.datatorrent.api.Operator;
 import com.datatorrent.stram.plan.logical.LogicalPlan;
 
+import scala.Function1;
 import scala.collection.Iterator;
 import scala.reflect.ClassTag;
 
@@ -14,22 +17,30 @@ public class ApexRDD<T> extends RDD<T>
 {
   private static final long serialVersionUID = -3545979419189338756L;
 
-  private DAG dag;
+  public DAG dag;
+  public Operator currentOperator;
+  public OperatorType currentOperatorType;
 
   public ApexRDD(RDD<T> rdd, ClassTag<T> classTag)
   {
     super(rdd, classTag);
   }
 
-  public ApexRDD()
+  public ApexRDD(ApexContext ac)
   {
-    super(null, null);
+    super(ac.emptyRDD((ClassTag<T>) scala.reflect.ClassManifestFactory.fromClass(Object.class)), (ClassTag<T>) scala.reflect.ClassManifestFactory.fromClass(Object.class));
     dag = new LogicalPlan();
   }
 
   public DAG getDag()
   {
     return dag;
+  }
+
+  @Override
+  public <U> RDD<U> map(Function1<T, U> f, ClassTag<U> evidence$3)
+  {
+    return null;
   }
 
   @Override
@@ -44,5 +55,11 @@ public class ApexRDD<T> extends RDD<T>
   {
     // TODO Auto-generated method stub
     return null;
+  }
+
+  public enum OperatorType {
+    INPUT,
+    PROCESS,
+    OUTPUT
   }
 }
